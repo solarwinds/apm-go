ea018e94e1466ec8d450fd86a4e8902beda75b77
tammy-baylis-swi <tammy.baylis@solarwinds.com>
2024-05-21 17:04:04 -0700

    More tests

--- internal/oboe/settings_lambda_test.go
+++ internal/oboe/settings_lambda_test.go
@@ -15,11 +15,15 @@
 package oboe
 
 import (
+	"os"
 	"testing"
 
 	"github.com/solarwinds/apm-go/internal/constants"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
+
+const SettingsFile = "/tmp/solarwinds-apm-settings.json"
 
 func TestNewSettingLambdaNormalized(t *testing.T) {
 	settingArgs := settingArguments{
@@ -96,4 +100,100 @@ func TestNewSettingLambdaNormalized(t *testing.T) {
 		[]byte{0x54, 0x4f, 0x4b, 0x45, 0x4e},
 		result.args[constants.KvSignatureKey],
 	)
+}
+
+func TestNewSettingLambdaFromFileErrorOpen(t *testing.T) {
+	require.NoFileExists(t, SettingsFile)
+	res, err := newSettingLambdaFromFile()
+	assert.Nil(t, res)
+	assert.Error(t, err)
+}
+
+func TestNewSettingLambdaFromFileErrorUnmarshal(t *testing.T) {
+	require.NoFileExists(t, SettingsFile)
+
+	content := []byte("hello\ngo\n")
+	os.WriteFile(SettingsFile, content, 0644)
+	res, err := newSettingLambdaFromFile()
+	assert.Nil(t, res)
+	assert.Error(t, err)
+
+	os.Remove(SettingsFile)
+}
+
+func TestNewSettingLambdaFromFileErrorLen(t *testing.T) {
+	require.NoFileExists(t, SettingsFile)
+
+	content := []byte("[]")
+	os.WriteFile(SettingsFile, content, 0644)
+	res, err := newSettingLambdaFromFile()
+	assert.Nil(t, res)
+	assert.Error(t, err)
+
+	os.Remove(SettingsFile)
+}
+
+func TestNewSettingLambdaFromFile(t *testing.T) {
+	require.NoFileExists(t, SettingsFile)
+
+	content := []byte("[{\"arguments\":{\"BucketCapacity\":1,\"BucketRate\":1,\"MetricsFlushInterval\":1,\"TriggerRelaxedBucketCapacity\":1,\"TriggerRelaxedBucketRate\":1,\"TriggerStrictBucketCapacity\":1,\"TriggerStrictBucketRate\":1},\"flags\":\"SAMPLE_START,SAMPLE_THROUGH_ALWAYS,SAMPLE_BUCKET_ENABLED,TRIGGER_TRACE\",\"layer\":\"\",\"timestamp\":1715900164,\"ttl\":120,\"type\":0,\"value\":1000000}]")
+	os.WriteFile(SettingsFile, content, 0644)
+	result, err := newSettingLambdaFromFile()
+	assert.Nil(t, err)
+	assert.Equal(t, int32(1), result.sType)
+	assert.Equal(t, "", result.layer)
+	assert.Equal(
+		t,
+		[]byte{0x53, 0x41, 0x4d, 0x50, 0x4c, 0x45, 0x5f, 0x53, 0x54, 0x41, 0x52, 0x54, 0x2c, 0x53, 0x41, 0x4d, 0x50, 0x4c, 0x45, 0x5f, 0x54, 0x48, 0x52, 0x4f, 0x55, 0x47, 0x48, 0x5f, 0x41, 0x4c, 0x57, 0x41, 0x59, 0x53, 0x2c, 0x53, 0x41, 0x4d, 0x50, 0x4c, 0x45, 0x5f, 0x42, 0x55, 0x43, 0x4b, 0x45, 0x54, 0x5f, 0x45, 0x4e, 0x41, 0x42, 0x4c, 0x45, 0x44, 0x2c, 0x54, 0x52, 0x49, 0x47, 0x47, 0x45, 0x52, 0x5f, 0x54, 0x52, 0x41, 0x43, 0x45},
+		result.flags,
+	)
+	assert.Equal(t, result.value, int64(1000000))
+	assert.Equal(t, result.ttl, int64(120))
+	assert.Equal(
+		t,
+		[]byte{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0xf0, 0x3f},
+		result.args[constants.KvBucketCapacity],
+	)
+	assert.Equal(
+		t,
+		[]byte{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0xf0, 0x3f},
+		result.args[constants.KvBucketRate],
+	)
+	assert.Equal(
+		t,
+		[]byte{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0xf0, 0x3f},
+		result.args[constants.KvTriggerTraceRelaxedBucketCapacity],
+	)
+	assert.Equal(
+		t,
+		[]byte{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0xf0, 0x3f},
+		result.args[constants.KvTriggerTraceRelaxedBucketRate],
+	)
+	assert.Equal(
+		t,
+		[]byte{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0xf0, 0x3f},
+		result.args[constants.KvTriggerTraceStrictBucketCapacity],
+	)
+	assert.Equal(
+		t,
+		[]byte{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0xf0, 0x3f},
+		result.args[constants.KvTriggerTraceStrictBucketRate],
+	)
+	assert.Equal(
+		t,
+		[]byte{0x1, 0x0, 0x0, 0x0},
+		result.args[constants.KvMetricsFlushInterval],
+	)
+	assert.Equal(
+		t,
+		[]byte(nil),
+		result.args[constants.KvMaxTransactions],
+	)
+	assert.Equal(
+		t,
+		[]byte{0x54, 0x4f, 0x4b, 0x45, 0x4e},
+		result.args[constants.KvSignatureKey],
+	)
+
+	os.Remove(SettingsFile)
 }
