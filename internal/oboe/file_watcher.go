79eb573feeb251416c0b215138b67607f7b5439f
tammy-baylis-swi <tammy.baylis@solarwinds.com>
2024-05-23 15:12:18 -0700

    Add FileBasedWatcher settingsCache

--- internal/oboe/file_watcher.go
+++ internal/oboe/file_watcher.go
@@ -15,53 +15,74 @@
 package oboe
 
 import (
+	"encoding/binary"
 	stdlog "log"
 	"time"
+
+	"github.com/coocood/freecache"
 )
 
 const (
-	tick     = 10
-	tickUnit = time.Second
+	cacheSize            = 5 * 1024 * 1024 // 5 MB
+	settingsCheckSeconds = 10
 )
 
+var keyTtl []byte = []byte("ttl")
 var exit = make(chan bool, 1)
 
 type FileBasedWatcher interface {
-	UpdateSettingFromFile()
 	Start()
 	Stop()
 }
 
 // NewFileBasedWatcher returns a FileBasedWatcher that periodically
-// does oboe.UpdateSetting using values from a settings JSON file.
+// does oboe.UpdateSetting using values from a settings JSON file,
+// if cached settings have not yet expired.
 func NewFileBasedWatcher(oboe *Oboe) FileBasedWatcher {
 	return &fileBasedWatcher{
 		*oboe,
+		*freecache.NewCache(cacheSize),
 	}
 }
 
 type fileBasedWatcher struct {
-	o Oboe
+	o             Oboe
+	settingsCache freecache.Cache
 }
 
-func (fbw *fileBasedWatcher) UpdateSettingFromFile() {
+// updateCacheFromFile sets "ttl" as byte representation of ttl in seconds.
+func (fbw *fileBasedWatcher) updateCacheFromFile(sl *settingLambdaNormalized) {
+	ttlBytes := make([]byte, 8)
+	binary.LittleEndian.PutUint64(ttlBytes, uint64(sl.ttl))
+	fbw.settingsCache.Set(keyTtl, ttlBytes, int(sl.ttl))
+}
+
+// updateSettingFromFile updates oboe settings using normalized settings from file.
+func (fbw *fileBasedWatcher) updateSettingFromFile(sl *settingLambdaNormalized) {
+	fbw.o.UpdateSetting(
+		sl.sType,
+		sl.layer,
+		sl.flags,
+		sl.value,
+		sl.ttl,
+		sl.args,
+	)
+}
+
+func (fbw *fileBasedWatcher) updateSettingAndCacheFromFile() {
 	settingLambda, err := newSettingLambdaFromFile()
 	if err != nil {
 		stdlog.Fatalf("Could not update setting from file: %s", err)
 		return
 	}
-	fbw.o.UpdateSetting(
-		settingLambda.sType,
-		settingLambda.layer,
-		settingLambda.flags,
-		settingLambda.value,
-		settingLambda.ttl,
-		settingLambda.args,
-	)
+	fbw.updateSettingFromFile(settingLambda)
+	fbw.updateCacheFromFile(settingLambda)
 }
 
+// Start runs a ticker that checks settings expiry from cache
+// and, if expired, updates cache and oboe settings.
 func (fbw *fileBasedWatcher) Start() {
-	ticker := time.NewTicker(tick * tickUnit)
+	ticker := time.NewTicker(settingsCheckSeconds * time.Second)
 	go func() {
 		defer ticker.Stop()
 		for {
@@ -69,8 +90,19 @@ func (fbw *fileBasedWatcher) Start() {
 			case <-exit:
 				return
 			case <-ticker.C:
-				stdlog.Print("Updating settings from file.")
-				fbw.UpdateSettingFromFile()
+				_, expireAt, err := fbw.settingsCache.GetWithExpiration(keyTtl)
+				if err != nil {
+					stdlog.Fatalf("There was an issue with settingsCache: %s", err)
+					// TODO: disable APM Go
+				} else {
+					// If cached settings expired, update cache and
+					// and update oboe settings
+					remainingTime := expireAt - uint32(time.Now().Unix())
+					if remainingTime <= 0 {
+						stdlog.Print("Updating settings from file.")
+						fbw.updateSettingAndCacheFromFile()
+					}
+				}
 			}
 		}
 	}()
