1b50691db65b57305bb68d59fdc0db7637edfdcc
Jared Harper <129781402+swi-jared@users.noreply.github.com>
2024-07-22 15:09:27 -0700

    Version 1.1.1-pre (#112)

--- internal/utils/version.go
+++ internal/utils/version.go
@@ -21,7 +21,7 @@ import (
 
 var (
 	// The SolarWinds Observability Go APM library version
-	version = "1.1.0"
+	version = "1.1.1-pre"
 
 	// The Go version
 	goVersion = strings.TrimPrefix(runtime.Version(), "go")
