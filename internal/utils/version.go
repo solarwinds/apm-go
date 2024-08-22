7484033b697a021514166022d2603f7f15987f44
Jared Harper <129781402+swi-jared@users.noreply.github.com>
2024-08-22 13:11:47 -0700

    Update version.go for post-v1.2.0 (#131)

--- internal/utils/version.go
+++ internal/utils/version.go
@@ -21,7 +21,7 @@ import (
 
 var (
 	// The SolarWinds Observability Go APM library version
-	version = "1.2.0"
+	version = "1.2.1-pre"
 
 	// The Go version
 	goVersion = strings.TrimPrefix(runtime.Version(), "go")
