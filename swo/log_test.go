3ec94cf088a0364684c3a403de1c4cc96029ae1b
Jared Harper <129781402+swi-jared@users.noreply.github.com>
2024-08-22 11:41:31 -0700

    [NH-86979] Add slog.Handler to associate trace context with logs (#129)

--- swo/log_test.go
+++ swo/log_test.go
@@ -15,11 +15,15 @@
 package swo
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"github.com/solarwinds/apm-go/internal/state"
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/otel/trace"
+	"log/slog"
 	"testing"
+	"time"
 )
 
 func TestLoggableTraceIDFromContext(t *testing.T) {
@@ -74,4 +78,77 @@ func TestLoggableTraceIDFromContext(t *testing.T) {
 	require.Equal(t,
 		"trace_id=33000000000000000000000000000000 span_id=aa00000000000000 trace_flags=01 resource.service.name=test-service",
 		lt.String())
+}
+
+func TestHandle(t *testing.T) {
+	prevServiceName := state.GetServiceName()
+	defer state.SetServiceName(prevServiceName)
+	serviceName := "test-service"
+	state.SetServiceName(serviceName)
+
+	type logLine struct {
+		Time        time.Time `json:"time"`
+		Level       string    `json:"level"`
+		Msg         string    `json:"msg"`
+		ServiceName string    `json:"resource.service.name"`
+		TraceID     string    `json:"trace_id"`
+		SpanID      string    `json:"span_id"`
+		TraceFlags  string    `json:"trace_flags"`
+	}
+
+	tests := []struct {
+		name     string
+		traceCtx LoggableTraceContext
+	}{
+		{"info level with trace", LoggableTraceContext{
+			TraceID:    trace.TraceID{0x11},
+			SpanID:     trace.SpanID{0x22},
+			TraceFlags: trace.FlagsSampled,
+		}},
+		{"empty trace", LoggableTraceContext{}},
+	}
+
+	now := time.Now().Truncate(time.Second).UTC()
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// arrange
+			writer := bytes.NewBuffer(make([]byte, 0))
+			baseHandler := slog.NewJSONHandler(writer, &slog.HandlerOptions{})
+			handler := NewLogHandler(baseHandler)
+
+			spanContext := trace.NewSpanContext(trace.SpanContextConfig{
+				TraceID:    tt.traceCtx.TraceID,
+				SpanID:     tt.traceCtx.SpanID,
+				TraceFlags: tt.traceCtx.TraceFlags,
+			})
+			ctx := trace.ContextWithSpanContext(context.Background(), spanContext)
+			record := slog.NewRecord(now, slog.LevelInfo, "test message", 0)
+
+			// act
+			err := handler.Handle(ctx, record)
+			require.NoError(t, err)
+
+			// assert
+			logOutput := writer.String()
+
+			var line logLine
+			err = json.Unmarshal([]byte(logOutput), &line)
+			require.NoError(t, err)
+
+			if tt.traceCtx.IsValid() {
+				require.Equal(t, tt.traceCtx.TraceID.String(), line.TraceID)
+				require.Equal(t, tt.traceCtx.SpanID.String(), line.SpanID)
+				require.Equal(t, tt.traceCtx.TraceFlags.String(), line.TraceFlags)
+			} else {
+				require.Empty(t, line.TraceID)
+				require.Empty(t, line.SpanID)
+				require.Empty(t, line.TraceFlags)
+			}
+			require.Equal(t, "test message", line.Msg)
+			require.Equal(t, serviceName, line.ServiceName)
+			require.Equal(t, now, line.Time)
+
+		})
+	}
 }
