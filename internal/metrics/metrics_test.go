631dd4543e96de1c9e1530c65f5c626105b4fa97
Grzegorz Glogowski <grzegorz.glogowski@solarwinds.com>
2025-05-26 13:40:24 +0200

    NH-108953 Fix for inaccurate tracecount metric value (#205)
    * fix tracecount metric
    * add missing license headers

--- internal/metrics/metrics_test.go
+++ internal/metrics/metrics_test.go
@@ -513,31 +513,6 @@ func TestEventQueueStats(t *testing.T) {
 	assert.Equal(t, original, *swapped)
 }
 
-func TestRateCounts(t *testing.T) {
-	rc := &RateCounts{}
-
-	rc.RequestedInc()
-	assert.EqualValues(t, 1, rc.Requested())
-
-	rc.SampledInc()
-	assert.EqualValues(t, 1, rc.Sampled())
-
-	rc.LimitedInc()
-	assert.EqualValues(t, 1, rc.Limited())
-
-	rc.TracedInc()
-	assert.EqualValues(t, 1, rc.Traced())
-
-	rc.ThroughInc()
-	assert.EqualValues(t, 1, rc.Through())
-
-	original := *rc
-	cp := rc.FlushRateCounts()
-
-	assert.Equal(t, original, *cp)
-	assert.Equal(t, &RateCounts{}, rc)
-}
-
 func TestRecordSpan(t *testing.T) {
 	tr, teardown := testutils.TracerSetup()
 	defer teardown()
